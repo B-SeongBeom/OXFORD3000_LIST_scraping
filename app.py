1d4cb7448dfa16b2796305eff96e343e0b6a05e8
SeongBeom <bsb082624@gmail.com>
2023-03-08 20:48:04 +0900

    requests beautifulsoup 활용 3000개 이상의 단어를 리스트로 만들기 완료

---
+++ app.py
@@ -0,0 +1,36 @@
+import requests
+from bs4 import BeautifulSoup
+
+# 알파벳 리스트
+oxford_urls = [
+    'https://www.oxfordlearnersdictionaries.com/wordlist/american_english/oxford3000/Oxford3000_A-B/?page=',
+    'https://www.oxfordlearnersdictionaries.com/wordlist/american_english/oxford3000/Oxford3000_C-D/?page=',
+    'https://www.oxfordlearnersdictionaries.com/wordlist/american_english/oxford3000/Oxford3000_E-G/?page=',
+    'https://www.oxfordlearnersdictionaries.com/wordlist/american_english/oxford3000/Oxford3000_H-K/?page=',
+    'https://www.oxfordlearnersdictionaries.com/wordlist/american_english/oxford3000/Oxford3000_L-N/?page=',
+    'https://www.oxfordlearnersdictionaries.com/wordlist/american_english/oxford3000/Oxford3000_O-P/?page=',
+    'https://www.oxfordlearnersdictionaries.com/wordlist/american_english/oxford3000/Oxford3000_Q-R/?page=',
+    'https://www.oxfordlearnersdictionaries.com/wordlist/american_english/oxford3000/Oxford3000_S/?page=',
+    'https://www.oxfordlearnersdictionaries.com/wordlist/american_english/oxford3000/Oxford3000_T/?page=',
+    'https://www.oxfordlearnersdictionaries.com/wordlist/american_english/oxford3000/Oxford3000_U-Z/?page='
+]
+
+# 로그 출력
+word_list = []
+
+for oxford_url in oxford_urls:
+    count = 1
+    while count < 5:
+        # print(alphabet_item + str(count))
+        response = requests.get(oxford_url + str(count), headers={'User-Agent':'Mozilla/5.0'})
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+
+        alphabet_list = soup.select('#entrylist1 > ul > li > a')
+        for alphabet_item in alphabet_list:
+            word_list.append(alphabet_item.text) 
+        count += 1
+
+print(word_list)
+print(len(word_list))
+
